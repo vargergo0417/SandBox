457edd674c03f91d827376bd2e26b28802532b2c
vargergo0417 <varga.gergo0417@gmail.com>
2020-02-06 12:40:23 +0100

    Commit2

--- src/content.ts
+++ src/content.ts
@@ -1,4 +1,9 @@
-﻿import fs from "fs";
+﻿/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-inferrable-types */
+/* eslint-disable no-var */
+/* eslint-disable prefer-const */
+/* eslint-disable prettier/prettier */
+import fs from "fs";
 import http from "http";
 import url from "url";
 
@@ -6,6 +11,9 @@ interface InputInterface {
     name: string;
     age: number;
     male: boolean;
+}
+function Hiba(): void {
+    console.log("Hiba");
 }
 export default class Content {
     public content(req: http.IncomingMessage, res: http.ServerResponse): void {
@@ -28,33 +36,34 @@ export default class Content {
 
         // Kezd a kódolást innen -->
 
-        res.write("Egyszerű Hello World!\n");
+        var szám: number = 123;
+        var márka: string = "Trabant";
+        var eldöntendő: boolean = false;
+        let Márkák: string[] = ["Seat", "VW", "Audi"];
+        let Italok: Array<string> = ["Cola", "Fanta", "Sprite"];
 
-        // Tetszőleges html teg-ek és attribútumok beépítése:
-        res.write("<span style='color: blue;'><i>Színes és dőlt Hello World!'</i></span>\n");
+        res.write(`${szám} ${márka} ${eldöntendő}`);
 
-        // Egyszerű input JSON állományból:
-        const input: InputInterface = JSON.parse(fs.readFileSync("input.json", "utf-8"));
+        var dolgozó: [number, string, boolean] = [1, "Gipsz Jakab", false];
+        res.write(`${dolgozó}`);
+        enum Tv {
+            Hossz = 130,
+            Magasság = 60,
+            Ár = 20000
+        }
 
-        // String template használata
-        res.write(`1. feladat: ${input.name} kora: ${input.age}\n`);
+        let teszt: string | number;
+        teszt = 1;
+        teszt = "Teszt";
 
-        // Változó definiálása:
-        let neme = "";
-        neme = input.male ? "férfi" : "nő";
-        res.write(`2. feladat: ${input.name} neme: ${neme}\n`);
+        let valami: any = "Jedlik győr";
+        valami = 23;
+        valami = true;
 
-        // Input form-al és <input type='number'>-el:
-        // URL paraméter(ek) (itt kor) ellenőrzése,  kiolvasása, alapértelmezett értéke 18:
-        const u = url.parse(req.url as string, true).query;
-        // ha "kor" paraméter nincs megadva vagy "kor" paraméter üres string,
-        // akkor legyen 18 az értéke, egyébként konvertáljuk számra a "kor" paraméter értékét:
-        // let kor: number = u.kor === undefined || u.kor === "" ? 18 : parseInt(u.kor as string);
-        let kor: number = parseInt(u.kor as string);
-        if (isNaN(kor) || kor < 0 || kor > 99) kor = 18; // egy kis ellenőrzés
-        res.write(`3. feladat: Kérem a korod [0-99]: <input type='text' name='kor' value=${kor} style='width:3em;'>\n`);
-        res.write(`4. feladat: Te ${kor} éves vagy!\n`);
+        let Error: void = Hiba();
+        console.log(Error);
 
+        res.write(`${Tv} ${teszt} ${valami}`);
         // <---- Fejezd be a kódolást
 
         res.write("</pre></form></body></html>");
